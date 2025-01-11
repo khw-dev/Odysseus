44d3de20000d4e665b8488da43477c0df0331b47
khw-dev <khw.developer@gmail.com>
2025-01-11 22:15:45 +0900

    feat: Add account creation page and unify style guide

--- app/src/main/java/com/example/ppet/Account.kt
+++ app/src/main/java/com/example/ppet/Account.kt
@@ -7,21 +7,29 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
 import android.widget.Button
+import android.widget.EditText
+import android.content.Context
 
-class MainActivity : AppCompatActivity() {
+class Account : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_account)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val button = findViewById<Button>(R.id.startBtn)
+        val button = findViewById<Button>(R.id.create_account)
         button.setOnClickListener {
-            val intent = Intent(this, Account::class.java)
+            val nickname = findViewById<EditText>(R.id.nickname).text.toString()
+            val sharedPref = getSharedPreferences("ppet", Context.MODE_PRIVATE)
+            with (sharedPref.edit()) {
+                putString("nickname", nickname)
+                apply()
+            }
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
